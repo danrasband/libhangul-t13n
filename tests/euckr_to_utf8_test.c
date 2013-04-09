4f684ee094b171cd5a60d4c67c811c2cc99a7afa
Dan Rasband <danrasband@gmail.com>
2013-04-09 14:10:58 -0600

    Add EUC-KR to UTF-8 conversion test

---
+++ tests/euckr_to_utf8_test.c
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+
+#include "../src/encoding.h"
+#include <stdio.h>
+#include <string.h>
+
+#define PASS 0
+#define FAIL 1
+
+int
+main()
+{
+    char *from_encoding = "EUC-KR";
+		char *to_encoding = "UTF-8";
+		// Orig is euc-kr-encoded "안녕\n"
+    char orig[] = {0xbe, 0xc8, 0xb3, 0xe7, 0x0a, '\0'};
+		char *encoded;
+		int pass = FAIL;
+		encoded = encode(orig, to_encoding, from_encoding);
+
+
+		if (0 == strncmp("안녕\n", encoded, strlen("안녕\n")))
+			 pass = PASS;
+
+		free(encoded);
+		return pass;
+}
+
